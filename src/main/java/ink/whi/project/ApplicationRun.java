f60906eea44a22809b429fe415054a4ba52166fd
qing-wq <2702461713@qq.com>
2023-12-07 23:40:45 +0800

    feat: 更新配置

--- src/main/java/ink/whi/project/ApplicationRun.java
+++ src/main/java/ink/whi/project/ApplicationRun.java
@@ -1,13 +1,17 @@
 package ink.whi.project;
 
+import ink.whi.project.hook.interceptor.AuthInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableAsync
 @EnableCaching
@@ -17,7 +21,17 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @MapperScan(basePackages = {"ink.whi.project.modules.user.repo",
         "ink.whi.project.modules.announcement.repo"})
 @EnableAspectJAutoProxy
-public class ApplicationRun {
+public class ApplicationRun implements WebMvcConfigurer {
+
+    @Autowired
+    private AuthInterceptor authInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor).addPathPatterns("/**")
+                .excludePathPatterns("/error");
+    }
+
     public static void main(String[] args) {
         new SpringApplicationBuilder(ApplicationRun.class).allowCircularReferences(true).run(args);
     }
